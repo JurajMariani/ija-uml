7d9bb42ea07e11646804fd0273cdd5844a8a0a00
Juraj Mariani <xmaria03@stud.fit.vutbr.cz>
2022-03-27 14:58:52 +0200

    Element Implementation

---
+++ src/uml/core/Element.java
@@ -0,0 +1,22 @@
+package uml.core;
+
+
+public class Element extends java.lang.Object
+{
+    String name;
+
+    public Element(String name)
+    {
+        this.name = name;
+    }
+
+    public String get_name()
+    {
+        return( this.name );
+    }
+
+    public void rename(String new_name)
+    {
+        this.name = new_name;
+    }
+}
