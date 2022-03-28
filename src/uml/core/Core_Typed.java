f10be8ef789de369c508e6270dd55075aa5f99c9
Juraj Mariani <xmaria03@stud.fit.vutbr.cz>
2022-03-28 22:07:43 +0200

    Merged C_Vis and C_Typed

--- src/uml/core/Core_Typed.java
+++ src/uml/core/Core_Typed.java
@@ -5,19 +5,24 @@ import uml.core.Element;
 
 public class Core_Typed extends Element
 {
-    String type;
-
+    //  0 -> public
+    //  1 -> protected
+    //  2 -> private
+    protected int visibility;
+    protected String type;
 
     public Core_Typed()
     {
         super();
         this.type = "";
+        this.visibility = 0;
     }
 
-    public Core_Typed(String name, String type)
+    public Core_Typed(String name, String type, int visibility)
     {
         super(name);
         this.type = type;
+        this.visibility = visibility;
     }
 
     public String get_type()
@@ -28,5 +33,33 @@ public class Core_Typed extends Element
     public void change_type(String new_type)
     {
         this.type = new_type;
+    }
+
+    public int get_visibility()
+    {
+        return( this.visibility );
+    }
+
+    public void change_visibility(int new_visibility)
+    {
+        this.visibility = new_visibility;
+    }
+
+    public String get_str_visibility()
+    {
+        switch( this.visibility )
+        {
+            case 0:
+                return( "+" );
+
+            case 1:
+                return( "#" );
+
+            case 2:
+                return( "-" );
+
+            default:
+                return( "/" );
+        }
     }
 }
