add8dded3de4311cf0b0b1655a05c0ce0e30c990
Juraj Mariani <xmaria03@stud.fit.vutbr.cz>
2022-04-26 22:24:46 +0200

    Some work done but still plenty to do

--- src/uml/seq/Seq_Class.java
+++ src/uml/seq/Seq_Class.java
@@ -2,18 +2,36 @@ package uml.seq;
 
 
 import uml.core.Element;
+import uml.core.Core_Method;
 import uml.core.Core_Class;
+import java.util.List;
 
 public class Seq_Class extends Element{
     
     protected Core_Class reference_class;
     protected static int instance;
+    protected List<Core_Method> messagelist;
 
 
     public Seq_Class(Core_Class ref)
     {
         super(ref.name);
 
+        /** This will, for now at least, stay as a shallow copy */
+        this.reference_class = ref;
+        this.messagelist = ref.get_methods();
         instance++;
     }
+
+    public int get_instance()
+    {
+        return instance;
+    }
+
+    public void update_sequence()
+    {
+        this.messagelist = this.reference_class.get_methods();
+    }
+
+    
 }
--- src/uml/seq/Seq_SequenceDiagram.java
+++ src/uml/seq/Seq_SequenceDiagram.java
@@ -3,6 +3,7 @@ package uml.seq;
 
 import uml.core.Element;
 import uml.core.Core_Class;
+import uml.core.Core_Method;
 import uml.core.Core_ClassDiagram;
 import seq.Seq_Message;
 import seq.Seq_Class;
@@ -16,19 +17,25 @@ public class Seq_SequenceDiagram extends Element{
     protected List<Core_Class> available_classes;
 
     protected List<Seq_SequenceClass> classlist;
-    protected List<Seq_Message> messagelist;
+    // moved to seq_class?
+    //protected List<Seq_Message> messagelist;
 
-    public Seq_SequenceDiagram(Core_ClassDiagram cd)
+    public Seq_SequenceDiagram(String name, Core_ClassDiagram cd)
     {
-        super();
+        super(name);
 
         this.available_classes = cd.get_classes();
 
         this.classlist = new ArrayList<Seq_SequenceClass>();
-        this.messagelist = new ArrayList<Seq_Message>();
+        Core_Class system = new Core_Class("System");
+        Core_Method meth = system.add_method();
+        meth.rename("init");
+        this.classlist.add(system);
+        //this.messagelist = new ArrayList<Seq_Message>();
     }
 
-    public boolean is_in_classlist(){
+    public boolean is_in_classlist()
+    {
         return true;
     };
 
@@ -51,11 +58,22 @@ public class Seq_SequenceDiagram extends Element{
         this.classlist.remove(actor);
     }
 
-    public Seq_Class get_actor(String name)
+    public Seq_Class get_actor_by_name(String name)
     {
         for (Seq_Class actor : this.classlist)
         {
             if( actor.name.equals(name) )
+                return actor;
+        }
+
+        return null;
+    }
+
+    public Seq_Class get_actor_by_id(int id)
+    {
+        for (Seq_Class actor : this.classlist)
+        {
+            if( actor.get_instance() == id )
                 return actor;
         }
 
